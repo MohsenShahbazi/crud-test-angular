cdb6fa73ff47978332f4692c3b4ebb5532693011
MohsenShahbazi <mohsenshahbaze@gmail.com>
2023-04-26 08:57:12 +0330

    develop one it block for sample unit test writing

--- src/app/customer/customer-list/customer-list.component.spec.ts
+++ src/app/customer/customer-list/customer-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomerListComponent } from './customer-list.component';
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('CustomerListComponent', () => {
   let component: CustomerListComponent;
@@ -19,7 +21,22 @@ describe('CustomerListComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    fixture.destroy();
+    TestBed.resetTestingModule();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should check addCustomer exist button and check click event', () => {
+    spyOn(component, 'addCustomer');
+    const createPositionButton: DebugElement = fixture.debugElement.query(By.css('#addCustomer'));
+    createPositionButton.triggerEventHandler('click', null);
+    expect(component.addCustomer).toHaveBeenCalled();
+  });
+
+
+
 });
